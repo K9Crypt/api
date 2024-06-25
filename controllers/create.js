86f3d76eae616bfa4432bc55c3bfa43a84b82c50
Fatih Yılmaz <60826916+Queaxtra@users.noreply.github.com>
2024-06-25 21:16:09 +0300

    bug fix

--- controllers/create.js
+++ controllers/create.js
@@ -5,25 +5,26 @@ const db = require('../database/db');
 
 /**
  * Asynchronous function to create and save an encrypted message.
- * 
+ *
  * @param {Object} req - The request object containing the message in the body.
  * @param {Object} res - The response object used to send back the appropriate HTTP response.
- * 
+ *
  * @returns {Promise<void>} - A promise that resolves to void.
  */
 async function create(req, res) {
     const { message } = req.body;
-    
+
     if (!message) {
         return res.status(400).send('Message is required');
     }
 
     const secretKey = process.env.SECRET_KEY || 'default-secret-key';
     const encryptedMessage = AES.encrypt(message, secretKey).toString();
+    const code = randomCode();
 
     try {
-        await db.set(randomCode(), encryptedMessage);
-        res.status(201).send('Message encrypted and saved successfully.');
+        await db.set(code, encryptedMessage);
+        res.status(201).send(`${code}`);
     } catch (error) {
         console.error('Error saving message:', error.message);
         res.status(500).send('Error saving message.');
