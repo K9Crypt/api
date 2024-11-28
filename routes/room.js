6ef1421749ab2f39379c7134c5b0f064c485bc19
Fatih Yılmaz <60826916+Queaxtra@users.noreply.github.com>
2024-11-28 20:59:23 +0300

    New features added.

--- routes/room.js
+++ routes/room.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createRoom, joinRoom, leaveRoom, sendMessage, getMessages, checkRoom, listAllRooms } = require('../controllers/roomController');
+const { createRoom, joinRoom, leaveRoom, sendMessage, getMessages, checkRoom, listAllRooms, markMessageAsRead, reactToMessage } = require('../controllers/roomController');
 
 router.post('/create', createRoom);
 router.post('/join', joinRoom);
@@ -9,5 +9,7 @@ router.post('/message', sendMessage);
 router.get('/:roomId/messages', getMessages);
 router.get('/check/:roomId', checkRoom);
 router.get('/list', listAllRooms);
+router.post('/read', markMessageAsRead);
+router.post('/react', reactToMessage);
 
 module.exports = router;
